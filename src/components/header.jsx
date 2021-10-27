0bd94de3f8dc04439f7426775e6364e169e7805f
Ryan Gibson <gibsorya@gmail.com>
2021-10-27 12:51:59 -0500

    Added navbar functionality

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,27 +6,30 @@ class Header extends Component {
     super(props);
     this.state = {
       scrolled: false,
+      active: false,
     };
   }
+
   componentDidMount() {
-    window.addEventListener("scroll", () => {
-      const isTop = window.scrollY < 100;
-      if (isTop !== true) {
-        this.setState({ scrolled: true });
-      } else {
-        this.setState({ scrolled: false });
-      }
+    const activeBtn = document.querySelectorAll(".btns");
+    const nav = document.querySelector("#myNav");
+
+    activeBtn.forEach(function(btn) {
+      btn.addEventListener("click", function () {
+        let current = document.querySelectorAll(".activeNav", "");
+        if(current[0]){
+          current[0].className = current[0].className.replace(" activeNav", "");
+          this.className += " activeNav";
+        } else {
+          this.className += " activeNav";
+        }
+      });
     });
   }
-
-  componentWillUnmount() {
-    window.removeEventListener("scroll  ");
-  }
-
   render() {
     return (
       <React.Fragment>
-        <nav className={this.state.scrolled ? "flex scrolled" : "flex"}>
+        {/* <nav className={this.state.scrolled ? "flex scrolled" : "flex"}>
           <div className="link-wrap">
             <div className="page-link" href="#home">
               Home
@@ -34,6 +37,15 @@ class Header extends Component {
             <div className="page-link">About</div>
             <div className="page-link">Projects</div>
             <div className="page-link">Contact</div>
+          </div>
+        </nav> */}
+        <nav className="scrollSticky">
+          <div id="activeButton">
+            <div className="topnav" id="myNav">
+              <a href="#home" className="btns">Home</a>
+              <a href="#about" className="btns">About</a>
+              
+            </div>
           </div>
         </nav>
       </React.Fragment>
