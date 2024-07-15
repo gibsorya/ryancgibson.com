493a71c020583af19c658d054d64a9dc77573139
Ryan Gibson <gibsorya@gmail.com>
2024-07-15 16:13:17 -0500

    init sanity

---
+++ sanity/schemaTypes/pageType.ts
@@ -0,0 +1,32 @@
+import { defineField, defineType } from "sanity";
+
+export const pageType = defineType({
+    name: 'page',
+    title: 'Page',
+    type: 'document',
+    fields: [
+        defineField({
+            name: 'title',
+            type: 'string'
+        }),
+        defineField({
+            name: 'slug',
+            type:'slug',
+            validation: (Rule) => Rule.required(),
+        }),
+        defineField({
+            name: 'content',
+            type: 'array',
+            of: [
+                {
+                    type: 'reference',
+                    to: [
+                        {
+                            type: 'page'
+                        },
+                    ]
+                }
+            ]
+        })
+    ]
+})
