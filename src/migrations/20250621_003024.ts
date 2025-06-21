de26adefb07b8ec8babf46ecbe09721ee489f962
Ryan Gibson <ryan@gibs.gg>
2025-06-20 19:36:05 -0500

    lint fix

--- src/migrations/20250621_003024.ts
+++ src/migrations/20250621_003024.ts
@@ -1,6 +1,6 @@
 import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-vercel-postgres'
 
-export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
+export async function up({ db }: MigrateUpArgs): Promise<void> {
   await db.execute(sql`
    CREATE TYPE "public"."enum_pages_blocks_project_background_color" AS ENUM('white', 'tomato-red', 'light_blue', 'rich_black', 'wenge-gray');
   CREATE TYPE "public"."enum__pages_v_blocks_project_background_color" AS ENUM('white', 'tomato-red', 'light_blue', 'rich_black', 'wenge-gray');
@@ -8,7 +8,7 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   ALTER TABLE "_pages_v_blocks_project" ADD COLUMN "background_color" "enum__pages_v_blocks_project_background_color" DEFAULT 'white';`)
 }
 
-export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
+export async function down({ db }: MigrateDownArgs): Promise<void> {
   await db.execute(sql`
    ALTER TABLE "pages_blocks_project" DROP COLUMN IF EXISTS "background_color";
   ALTER TABLE "_pages_v_blocks_project" DROP COLUMN IF EXISTS "background_color";
