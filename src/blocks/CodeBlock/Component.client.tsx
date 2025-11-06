2d202b22d53ec5cb9a89d611f005a85b87f4f45f
Ryan Gibson <ryan@gibs.gg>
2025-11-06 15:21:52 -0600

    fix custom code import errors from prismjs

--- src/blocks/CodeBlock/Component.client.tsx
+++ src/blocks/CodeBlock/Component.client.tsx
@@ -8,17 +8,21 @@ type Props = {
   language?: string
 }
 
+/* eslint-disable */
 (typeof global !== "undefined" ? global : window).Prism = Prism
+// @ts-ignore
 await import("prismjs/components/prism-ruby")
+// @ts-ignore
 await import("prismjs/components/prism-css")
+/* eslint-enable */
 
 export const Code: React.FC<Props> = ({ code, language = '' }) => {
   if (!code) return null
 
   return (
-    <Highlight code={code} language={language} theme={themes.vsDark}>
+    <Highlight prism={Prism} code={code} language={language} theme={themes.gruvboxMaterialDark}>
       {({ getLineProps, getTokenProps, tokens }) => (
-        <pre className="code-block text-xs sm:text-base p-4 border border-border rounded overflow-x-auto">
+        <pre className="code-block text-xs sm:text-base p-4 border-3 border-wenge-gray rounded overflow-x-auto">
           {tokens.map((line, i) => (
             <div key={i} {...getLineProps({ className: 'table-row', line })}>
               <span className="table-cell select-none text-right text-white/25">{i + 1}</span>
